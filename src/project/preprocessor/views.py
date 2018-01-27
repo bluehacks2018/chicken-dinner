055b421c68dcc5bd7dad246b20263e8d0cf97b05
antonsuba <antonsuba@outlook.com>
2018-01-28 01:16:29 +0800

    Implement data encoding and coversion mechanism

--- src/project/preprocessor/views.py
+++ src/project/preprocessor/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+import csv
 
-# Create your views here.
+class PreProcessor():
+    def format_csv(self, name, url, desc, csv_file):
+        json_data = {
+            'name': name,
+            'url': url,
+            'description': desc,
+            'rating': 0,
+            'popularity': 0,
+            'score': 0,
+            'data': {},
+        }
+
+        with open(csv_file) as f:
+            reader = csv.reader(f, delimiter=',')
+
+            row_num = 0
+            for row in reader:
+                # row = unicode(str, errors='ignore')
+                # print(row)
+                if row_num == 0:
+                    label_row = []
+                    for item in row:
+                        label_row.append(item)
+
+                    json_data['data']['labels'] = label_row
+                    json_data['data']['series'] = []
+
+                else:
+                    item_counter = 0
+                    class_name = ""
+                    name = ""
+                    data = []
+
+                    for item in row:
+                        if item_counter == 0:
+                            class_name = str(item)
+                            name = str(item)
+                        else:
+                            data.append(item)
+
+                        item_counter += 1
+                    
+                    json_data['data']['series'].append({
+                        'className': class_name,
+                        'name': name,
+                        'data': data
+                    })
+
+                row_num += 1
+
+        return json_data
+
+preprocessor = PreProcessor()
+preprocessor.format_csv('DOF', 'url', 'desc', 'dof.csv')
