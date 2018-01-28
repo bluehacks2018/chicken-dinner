52592e6841b07c39d4932caf7fc5ef5a913ea22f
Fiel Faustino <thaf.faustino@gmail.com>
2018-01-28 10:48:47 +0800

    Added csv data files

--- src/project/preprocessor/views.py
+++ src/project/preprocessor/views.py
@@ -4,13 +4,78 @@ from project.dashboard.models import Dataset
 
 # class PreProcessor():
 def test_save_data(request):
-    save_data('DOF', 'url', 'desc', '/src/project/preprocessor/dof.csv')
+    save_data(
+        'Internal Revenue Allotment - Region V', 
+        'https://data.gov.ph/sites/default/files/albay_legazpi_city_ira_utilization_122012_q4.csv', 
+        'This dataset contains the quarterly submitted of the utilization of Special Development Funds documents of Region V',
+        'Finance',
+        'DILG',
+        'bar',
+        3,
+        250,
+        8,
+        '/src/project/preprocessor/datasets/albay_legazpi_city_ira_utilization_122012_q4.csv'
+    )
+
+    save_data(
+        'State University and Colleges - Faculty-Student Ratio', 
+        'https://data.gov.ph/sites/default/files/State%20Universities%20and%20Colleges%20Faculty-Student%20Ratio%20AY%202016-17.csv', 
+        'This dataset contains State Universities and College (SUCs) Faculty Student Ratio',
+        'Education',
+        'CHED',
+        'bar',
+        2,
+        501,
+        10,
+        '/src/project/preprocessor/datasets/State Universities and Colleges Faculty-Student Ratio AY 2016-17.csv'
+    )
+
+    # save_data(
+    #     'Balance Sheet and Key Ratios - Philippine Banking System', 
+    #     'https://data.gov.ph/sites/default/files/bsp_balance_sheet_key_ratios_2014.csv', 
+    #     'Balance Sheet/Consolidated Statement of Condition and Key Ratios of Philippine Banking System (PBS)',
+    #     'Business',
+    #     'BSP',
+    #     'bar',
+    #     5,
+    #     1000000,
+    #     10,
+    #     '/src/project/preprocessor/datasets/bsp_balance_sheet_key_ratios_2014.csv'
+    # )
+
+    save_data(
+        'NSCB Health Expenditure by Source of Funds', 
+        'https://data.gov.ph/sites/default/files/nscb_health_expenditure_by_source_of_funds_2009-2011.CSV', 
+        'This dataset contains NSCB Health Expenditure by source of funds. *Amount in million pesos.',
+        'Health',
+        'PSA',
+        'bar',
+        4,
+        684253,
+        7,
+        '/src/project/preprocessor/datasets/nscb_health_expenditure_by_source_of_funds_2009-2011.csv'
+    )
+
+    save_data(
+        'NSCB Awareness of Laws providing protection to Land', 
+        'https://data.gov.ph/sites/default/files/nscb_awareness_of_laws_providing_protection_to_land_2012.csv',
+        'This dataset contains NSCB Awareness of Laws providing protection to Land. Percent of Respondents Aware of Laws. Total may exceed one hundred percent due to multiple responses',
+        'Environment',
+        'PSA',
+        'bar',
+        4,
+        572,
+        5,
+        '/src/project/preprocessor/datasets/nscb_awareness_of_laws_providing_protection_to_land_2012.csv'
+    )
+
+    # save_data('DOF', 'url', 'desc', '/src/project/preprocessor/datasets/dof.csv')
     return HttpResponse('OK')
 
-def save_data(name, url, desc, data_file):
+def save_data(name, url, desc, tag, gov_org, chart_type, rating, popularity, score, data_file):
     json_data = format_csv(data_file)
 
-    dataset = Dataset(name=name, url=url, description=desc, chart_type='bar', rating=0, popularity=0, score=0, data=json_data)
+    dataset = Dataset(name=name, url=url, description=desc, tag=tag, gov_org=gov_org, chart_type=chart_type, rating=rating, popularity=popularity, score=score, data=json_data)
     dataset.save()
 
 def format_csv(csv_file):
