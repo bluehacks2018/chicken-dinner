219380d5ee8e4b4567453a3509ae14853e854d44
Fiel Faustino <thaf.faustino@gmail.com>
2018-01-28 11:36:37 +0800

    Updated csv file list

--- src/project/preprocessor/views.py
+++ src/project/preprocessor/views.py
@@ -18,45 +18,6 @@ def test_save_data(request):
     )
 
     save_data(
-        'State University and Colleges - Faculty-Student Ratio', 
-        'https://data.gov.ph/sites/default/files/State%20Universities%20and%20Colleges%20Faculty-Student%20Ratio%20AY%202016-17.csv', 
-        'This dataset contains State Universities and College (SUCs) Faculty Student Ratio',
-        'Education',
-        'CHED',
-        'bar',
-        2,
-        501,
-        10,
-        '/src/project/preprocessor/datasets/State Universities and Colleges Faculty-Student Ratio AY 2016-17.csv'
-    )
-
-    # save_data(
-    #     'Balance Sheet and Key Ratios - Philippine Banking System', 
-    #     'https://data.gov.ph/sites/default/files/bsp_balance_sheet_key_ratios_2014.csv', 
-    #     'Balance Sheet/Consolidated Statement of Condition and Key Ratios of Philippine Banking System (PBS)',
-    #     'Business',
-    #     'BSP',
-    #     'bar',
-    #     5,
-    #     1000000,
-    #     10,
-    #     '/src/project/preprocessor/datasets/bsp_balance_sheet_key_ratios_2014.csv'
-    # )
-
-    save_data(
-        'NSCB Health Expenditure by Source of Funds', 
-        'https://data.gov.ph/sites/default/files/nscb_health_expenditure_by_source_of_funds_2009-2011.CSV', 
-        'This dataset contains NSCB Health Expenditure by source of funds. *Amount in million pesos.',
-        'Health',
-        'PSA',
-        'bar',
-        4,
-        684253,
-        7,
-        '/src/project/preprocessor/datasets/nscb_health_expenditure_by_source_of_funds_2009-2011.csv'
-    )
-
-    save_data(
         'NSCB Awareness of Laws providing protection to Land', 
         'https://data.gov.ph/sites/default/files/nscb_awareness_of_laws_providing_protection_to_land_2012.csv',
         'This dataset contains NSCB Awareness of Laws providing protection to Land. Percent of Respondents Aware of Laws. Total may exceed one hundred percent due to multiple responses',
@@ -67,6 +28,45 @@ def test_save_data(request):
         572,
         5,
         '/src/project/preprocessor/datasets/nscb_awareness_of_laws_providing_protection_to_land_2012.csv'
+    )
+
+    save_data(
+        'Metro Rail Transit Line 3 - Passenger Traffic (Saturdays)', 
+        'https://data.gov.ph/sites/default/files/2011_saturdays_ave.csv',
+        'This dataset contains the saturdays number of passengers who have entered and exited per station by average, percentage and total in MRT Line 3',
+        'Transportation',
+        'MRT Line 3',
+        'line',
+        5,
+        7384,
+        1,
+        '/src/project/preprocessor/datasets/2011_saturdays_ave.csv'
+    )
+
+    save_data(
+        'Climatological Normal Values', 
+        'https://data.gov.ph/sites/default/files/pagasanormvalalabatquez1999-2010.csv',
+        'Period averages computed for a uniform and relative long period comprising at least three (3) consecutive 10-year period.',
+        'Environment',
+        'PAGASA',
+        'bar',
+        4,
+        4125,
+        8,
+        '/src/project/preprocessor/datasets/pagasanormvalalabatquez1999-2010.csv'
+    )
+
+    save_data(
+        'Administration of Probation Systems and Parole Systems', 
+        'https://data.gov.ph/sites/default/files/ppa_pardon_supervision_caseload_1989-2013.csv',
+        'This dataset contains the total number of investigation of court referrals for the applicant or petitioner for probation. The post-sentence investigation is conducted to provide the courts with relevant information and judicious recommendations for the selection of offenders to be placed on probation. It also includes number pre-parole/executive clemency investigation conducted to provide the Board (Board of Pardons and Parole) with necessary and relevant information which it can use in determining a prisoner’s fitness for parole or any form of executive clemency. Moreover, it contains the number of probationers, parolees, and pardonees supervised by the Agency to effect the rehabilitation and reintegration of these clients (i.e.,probationers, parolees, and pardonees) as productive, law-abiding and socially responsible members of the community. It also shows the Courts\' and Boards\' actions on the investigation and supervision reports submitted to them by the Agency.',
+        'Justice',
+        'Parole and Probation Administration',
+        'bar',
+        5,
+        52389,
+        9,
+        '/src/project/preprocessor/datasets/ppa_pardon_supervision_caseload_1989-2013.csv'
     )
 
     # save_data('DOF', 'url', 'desc', '/src/project/preprocessor/datasets/dof.csv')
